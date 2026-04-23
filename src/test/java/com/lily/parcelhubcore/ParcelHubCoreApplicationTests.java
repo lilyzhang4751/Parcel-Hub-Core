82c530811f1def13d3a52231a273a7eed8312e0d
“lilyzhang4751” <zhangli4751@163.com>
2026-04-23 20:41:15 +0800

    delete @SpringBootTest

--- src/test/java/com/lily/parcelhubcore/ParcelHubCoreApplicationTests.java
+++ src/test/java/com/lily/parcelhubcore/ParcelHubCoreApplicationTests.java
@@ -1,23 +1,8 @@
 package com.lily.parcelhubcore;
 
-import com.lily.parcelhubcore.parcel.infrastructure.persistence.repository.WaybillRegistryRepository;
-import com.lily.parcelhubcore.user.application.service.LoginService;
-import com.lily.parcelhubcore.user.infrastructure.persistence.repository.UserInfoRepository;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class ParcelHubCoreApplicationTests {
-
-    @Resource
-    private WaybillRegistryRepository waybillRegistryRepository;
-
-    @Resource
-    private UserInfoRepository userInfoRepository;
-
-    @Resource
-    private LoginService loginService;
 
     @Test
     void contextLoads() {
