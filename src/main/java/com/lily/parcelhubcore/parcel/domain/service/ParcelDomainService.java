b8cddb1a81d0827d97a88da256ded6644fd1ab29
“lilyzhang4751” <zhangli4751@163.com>
2026-05-08 12:05:50 +0800

    add further idempotency in consumer

--- src/main/java/com/lily/parcelhubcore/parcel/domain/service/ParcelDomainService.java
+++ src/main/java/com/lily/parcelhubcore/parcel/domain/service/ParcelDomainService.java
@@ -16,7 +16,6 @@ import com.lily.parcelhubcore.parcel.infrastructure.persistence.repository.Waybi
 import com.lily.parcelhubcore.shared.enums.WaybillRegistryStatusEnum;
 import com.lily.parcelhubcore.shared.enums.WaybillStatusEnum;
 import com.lily.parcelhubcore.shared.exception.BusinessException;
-import com.lily.parcelhubcore.user.application.service.MobileCryptoService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
